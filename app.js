315bc4a0dd4eeeb7e3fef5d823be8e728a820b84
Mario Posada <marioposadamiranda@gmail.com>
2022-12-12 09:41:52 -0300

    Add request patch

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
 const express = require('express');
+const nodemon = require('nodemon');
 
 const app = express();
 
@@ -52,7 +53,6 @@ app.get('/api/v1/tours/:id', (req, res) => {
 
 app.post('/api/v1/tours', (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
-
   const newTour = Object.assign({ id: newId }, req.body);
   console.log(newTour);
 
@@ -71,6 +71,26 @@ app.post('/api/v1/tours', (req, res) => {
     }
   );
 });
+
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+
+  if(req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+res.status(200).json({
+  status: 'success',
+  data: {
+    tour: '<Updated tour here...>',
+  },
+});
+})
+
+
 
 const port = 3000;
 app.listen(port, () => {
