5bf9e95185c97e796ca23143b18feffebad61cdd
Mario Posada <marioposadamiranda@gmail.com>
2022-12-12 12:57:10 -0300

    Add Delete route

--- app.js
+++ app.js
@@ -90,6 +90,22 @@ res.status(200).json({
 });
 })
 
+app.delete('/api/v1/tours/:id', (req, res) => {
+
+  if(req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+res.status(204).json({
+  status: 'success',
+  data: {
+    tour: null
+  },
+});
+})
 
 
 const port = 3000;
