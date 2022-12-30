e596bf997a9a9826145dd9be6cecbb370602f120
Mario Posada <marioposadamiranda@gmail.com>
2022-12-30 16:19:07 -0300

    Fix middleware

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,6 +4,9 @@ const tourController = require('./../controllers/tourControllers');
 const router = express.Router();
 
 router
+.param('id', tourController.checkID)
+
+router
 .route('/')
 .get(tourController.getAllTours)
 .post(tourController.createTour);
