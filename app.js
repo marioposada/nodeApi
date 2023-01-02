7d3774c384784d00c5a99876a8263aa6311e8cad
Mario Posada <marioposadamiranda@gmail.com>
2023-01-02 17:17:27 -0300

    Fix dotenv

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+
 app.use(morgan('dev'));
 app.use(express.json());
 
