0ea5ba05ef7fc2d183690c17984effd9b591c0c9
Mario Posada <marioposadamiranda@gmail.com>
2023-01-02 17:49:30 -0300

    Add start:prod to package.json

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 const app = require('./app')
 
-dotenv.config({ path: './config.env' });
 
-console.log(process.env);
+// console.log(process.env.NODE_ENV);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
