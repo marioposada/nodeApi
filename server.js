9950a721bb45386a2fe71ada229ecf901073ff74
Mario Posada <marioposadamiranda@gmail.com>
2023-01-06 12:38:57 -0300

    Fix mongoose

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB).then((con) => {
+  console.log('DB connection successful!');
+  console.log(con.connections);
+});
+
 const app = require('./app');
 
 // console.log(process.env.NODE_ENV);
