9bc6e2583cf0cf01fc6abef4efb1aa2db9270604
Mario Posada <marioposadamiranda@gmail.com>
2023-01-09 19:33:12 -0300

    Delete function checkbody

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -1,15 +1,5 @@
 const Tour = require('./../models/tourModel');
 
-exports.Body = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name or price',
-    });
-  }
-  next();
-};
-
 exports.getAllTours = (req, res) => {
   // res.status(200).json({
   //   status: 'success',
@@ -33,22 +23,9 @@ exports.getTour = (req, res) => {
 };
 
 exports.createTour = (req, res) => {
-  // const newId = tours[tours.length - 1].id + 1;
-  // const newTour = Object.assign({ id: newId }, req.body);
-  // console.log(newTour);
-  // tours.push(newTour);
-  // fs.writeFile(
-  //   `${__dirname}/dev-data/data/tours-simple.json`,
-  //   JSON.stringify(tours),
-  //   () => {
-  //     res.status(201).json({
-  //       status: 'success',
-  //       data: {
-  //         tour: newTour,
-  //       },
-  //     });
-  //   }
-  // );
+  res.status(201).json({
+    status: 'success',
+  });
 };
 
 exports.updateTour = (req, res) => {
