941ae3499a2af7f71e1d25659fc312b08f4457b8
Mario Posada <marioposadamiranda@gmail.com>
2023-01-11 22:31:35 -0300

    Add get

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -3,7 +3,7 @@ const Tour = require('../models/tourModel');
 exports.getAllTours = async (req, res) => {
   try {
     const tours = await Tour.find();
-    console.log('Hasta aqui',tours);
+    console.log('Hasta aqui', tours);
     res.status(200).json({
       status: 'success',
       results: tours.length,
@@ -19,16 +19,21 @@ exports.getAllTours = async (req, res) => {
   }
 };
 
-exports.getTour = (req, res) => {
-  // const id = req.params.id * 1; // *1 converts string to number
-  // const tour = tours.find((el) => el.id === id);
-  // res.status(200).json({
-  //   status: 'success',
-  //   results: tours.length,
-  //   data: {
-  //     tours: tour,
-  //   },
-  // });
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour: tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 exports.createTour = async (req, res) => {
