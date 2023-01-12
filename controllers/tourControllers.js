d69a24db524e5926711c4e8e919f3422b006a771
Mario Posada <marioposadamiranda@gmail.com>
2023-01-11 22:31:11 -0300

    Fix state

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -1,6 +1,23 @@
 const Tour = require('../models/tourModel');
 
-exports.getAllTours = (req, res) => {};
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    console.log('Hasta aqui',tours);
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours: tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
 
 exports.getTour = (req, res) => {
   // const id = req.params.id * 1; // *1 converts string to number
