b25a19bb68e37b61a240e9d41063c02b4a5d1214
Mario Posada <marioposadamiranda@gmail.com>
2023-01-17 15:32:58 -0300

    Add Limiting

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -23,6 +23,14 @@ exports.getAllTours = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
+    // 4) Field limiting
+    if (req.query.fields) {
+      let fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
     // EXECUTE QUERY
     const tours = await query;
 
