e4a94e59ce1e782fd6ed89eac1c1d263ed55ea2d
Mario Posada <marioposadamiranda@gmail.com>
2023-01-20 12:05:41 -0300

    Add sort

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -129,6 +129,9 @@ exports.getTourStats = async (req, res) => {
           maxPrice: { $max: '$price' },
         },
       },
+      {
+        $sort: { avgPrice: 1 },
+      },
     ]);
     res.status(200).json({
       status: 'success',
