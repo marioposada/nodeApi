39335652708224e16494de31cdc0e2dec71e6e3f
Mario Posada <marioposadamiranda@gmail.com>
2023-01-20 13:17:14 -0300

    Fix it

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -157,6 +157,14 @@ exports.getMonthlyPlan = async (req, res) => {
       {
         $unwind: '$startDates',
       },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
     ]);
     res.status(200).json({
       status: 'success',
