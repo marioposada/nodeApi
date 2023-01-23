5319d202ec3c2a120826566f8abe7e23725965f2
Mario Posada <marioposadamiranda@gmail.com>
2023-01-23 16:47:56 -0300

    Add error 404

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -1,5 +1,6 @@
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 const APIFeatures = require('../utils/apiFeatures');
 /*
@@ -34,6 +35,11 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 
 exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
+
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
+
   res.status(200).json({
     status: 'success',
     data: {
