ddb9e73e90aecc9995d9e28d0bfa2904621487eb
Mario Posada <marioposadamiranda@gmail.com>
2023-01-23 09:29:24 -0300

    Add error middleware

--- app.js
+++ app.js
@@ -14,7 +14,26 @@ if (process.env.NODE_ENV === 'development') {
 app.use(morgan('dev'));
 app.use(express.json());
 
+// 3) ROUTES
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+  next();
+});
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 
 module.exports = app;
