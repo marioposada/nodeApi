a91ddcb0f89a57ad43eda5a8be46f12b65dc9ef8
Mario Posada <marioposadamiranda@gmail.com>
2023-01-23 16:54:36 -0300

    Fix !null

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -64,6 +64,10 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     new: true,
     runValidators: true,
   });
+
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
   res.status(200).json({
     status: 'success',
     data: {
@@ -74,6 +78,9 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findByIdAndDelete(req.params.id);
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
   res.status(204).json({
     status: 'success',
     data: {
