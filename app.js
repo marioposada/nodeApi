bba40c0aa76e60f907bd4441b62f791c47c4c204
Mario Posada <marioposadamiranda@gmail.com>
2023-01-23 10:31:08 -0300

    Add Error class

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -20,11 +21,12 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`,
-  });
-  next();
+  //
+  // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  // err.statusCode = 404;
+  // err.status = 'fail';
+
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 app.use((err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
